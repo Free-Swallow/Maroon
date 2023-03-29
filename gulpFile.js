77f9f25e347e01fb507ad7d1f9a6ed4dc329357c
Free-Swallow <nickphoenix21@icloud.com>
2023-03-29 20:56:30 +0700

    настройка среды разработки

---
+++ gulpFile.js
@@ -0,0 +1,45 @@
+import gulp from "gulp";
+import {create as bsCreate} from 'browser-sync';
+import pug from "gulp-pug";
+import {deleteSync} from "del";
+
+const browser = bsCreate();
+
+export const pugConvert = () => {
+  return gulp.src("src/*.pug", {base: "src"})
+    .pipe(pug({}))
+    .pipe(gulp.dest("build"));
+};
+
+export const clean = async () => {
+  return deleteSync("build");
+};
+
+export const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: "build",
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+};
+
+export const reload = (done) => {
+  browser.reload();
+  done();
+};
+
+export const watcher = () => {
+  gulp.watch("src/*.pug", gulp.series(pugConvert, reload));
+  gulp.watch("src/style/*.css", gulp.series(reload));
+};
+
+export default gulp.series(
+  clean,
+  pugConvert,
+  server,
+  watcher,
+);
