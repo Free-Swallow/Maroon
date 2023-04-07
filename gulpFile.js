68ce1c75698103e5e28038c482f2ad85c531e7a6
Free-Swallow <nickphoenix21@icloud.com>
2023-04-07 16:52:14 +0700

    test

--- gulpFile.js
+++ gulpFile.js
@@ -1,7 +1,4 @@
 import gulp from "gulp";
-import rename from 'gulp-rename';
-import terser from 'gulp-terser';
-import plumber from 'gulp-plumber';
 import {create as bsCreate} from 'browser-sync';
 import pug from "gulp-pug";
 import {deleteSync} from "del";
@@ -10,17 +7,8 @@ const browser = bsCreate();
 
 export const pugConvert = () => {
   return gulp.src("src/*.pug", {base: "src"})
-    .pipe(plumber())
     .pipe(pug({}))
     .pipe(gulp.dest("build"));
-};
-
-export const scripts = () => {
-  return gulp.src("src/js/main.js", {sourcemaps: true})
-    .pipe(terser())
-    .pipe(rename("main.min.js"))
-    .pipe(gulp.dest("build/js", {sourcemaps: '.'}))
-    .pipe(browser.stream());
 };
 
 export const clean = async () => {
@@ -30,7 +18,6 @@ export const clean = async () => {
 export const copy = (done) => {
   gulp.src([
     "src/img/svg/*.svg",
-    "src/img/*.{png,jpg,svg,webp,avif}",
   ], {
     base: "src"
   })
@@ -59,16 +46,12 @@ export const watcher = () => {
   gulp.watch("src/*.pug", gulp.series(pugConvert, reload));
   gulp.watch("src/**/*.pug", gulp.series(pugConvert, reload));
   gulp.watch("src/style/*.css", gulp.series(reload));
-  gulp.watch("src/js/*.js", gulp.series(scripts, reload));
 };
 
 export default gulp.series(
   clean,
   copy,
-  gulp.parallel(
-    pugConvert,
-    scripts,
-    server,
-    watcher,
-  )
+  pugConvert,
+  server,
+  watcher,
 );
