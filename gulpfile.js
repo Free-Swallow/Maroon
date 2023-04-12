fa4703fb434540472ec837d9f65c522f9dc01675
Free-Swallow <nickphoenix21@icloud.com>
2023-04-12 22:23:26 +0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 import gulp from "gulp";
+import rename from 'gulp-rename';
 import plumber from 'gulp-plumber';
+import sass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
 import {create as bsCreate} from 'browser-sync';
+import sourcemap from 'gulp-sourcemaps';
 import pug from "gulp-pug";
 import {deleteSync} from "del";
 
@@ -11,6 +17,20 @@ export const pugConvert = () => {
     .pipe(plumber())
     .pipe(pug({}))
     .pipe(gulp.dest("build"));
+};
+
+export const style = () => {
+  return gulp.src("src/sass/style.scss", {sourcemap: true})
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/style", {sourcemap: '.'}))
+    .pipe(browser.stream());
 };
 
 export const clean = async () => {
@@ -47,12 +67,13 @@ export const reload = (done) => {
 export const watcher = () => {
   gulp.watch("src/*.pug", gulp.series(pugConvert, reload));
   gulp.watch("src/**/*.pug", gulp.series(pugConvert, reload));
-  gulp.watch("src/style/*.css", gulp.series(reload));
+  gulp.watch("src/sass/**/*.scss", gulp.series(style, reload));
 };
 
 export default gulp.series(
   clean,
   copy,
+  style,
   pugConvert,
   server,
   watcher,
